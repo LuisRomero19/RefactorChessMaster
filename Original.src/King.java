4eb1db613b13d465b4502d04e49df85257aef9c0
LuisRomero19 <luis.romero20@unmsm.edu.pe>
2023-03-08 16:49:00 -0500

    nuevoavance

--- Original.src/King.java
+++ Original.src/King.java
@@ -1,56 +1,93 @@
 import javax.swing.ImageIcon;
 import java.util.ArrayList;
-
-public class King extends ChessGamePiece {
-    public King(ChessGameBoard board, int row, int col, int color) {
-        super(board, row, col, color, false);
+// -------------------------------------------------------------------------
+/**
+ * Represents a King game piece.
+ *
+ * @author Ben Katz (bakatz)
+ * @author Myles David II (davidmm2)
+ * @author Danielle Bushrow (dbushrow)
+ * @version 2010.11.17
+ */
+public class King
+    extends ChessGamePiece{
+    // ----------------------------------------------------------
+    /**
+     * Create a new King object.
+     *
+     * @param board
+     *            the board to create the king on
+     * @param row
+     *            the row to create the king on
+     * @param col
+     *            the column to create the king on
+     * @param color
+     *            either GamePiece.WHITE, BLACK, or UNASSIGNED
+     */
+    public King( ChessGameBoard board, int row, int col, int color ){
+        super( board, row, col, color, false );
     }
-
+    /**
+     * Calculates the possible moves for this piece. These are ALL the possible
+     * moves, including illegal (but at the same time valid) moves.
+     *
+     * @param board
+     *            the game board to calculate moves on
+     * @return ArrayList<String> the moves
+     */
     @Override
-    protected ArrayList<String> calculatePossibleMoves(ChessGameBoard board) {
-        ArrayList<String> possibleMoves = new ArrayList<String>();
-        int[] directions = {-1, 0, 1};
-
-        for (int dx : directions) {
-            for (int dy : directions) {
-                if (dx == 0 && dy == 0) {
-                    continue;
-                }
-                int newRow = getRow() + dx;
-                int newCol = getCol() + dy;
-                if (isValidMove(newRow, newCol)) {
-                    possibleMoves.add(getPositionString(newRow, newCol));
-                }
-            }
+    protected ArrayList<String> calculatePossibleMoves( ChessGameBoard board ){
+        ArrayList<String> northEastMoves = calculateNorthEastMoves( board, 1 );
+        ArrayList<String> northWestMoves = calculateNorthWestMoves( board, 1 );
+        ArrayList<String> southEastMoves = calculateSouthEastMoves( board, 1 );
+        ArrayList<String> southWestMoves = calculateSouthWestMoves( board, 1 );
+        ArrayList<String> northMoves = calculateNorthMoves( board, 1 );
+        ArrayList<String> southMoves = calculateSouthMoves( board, 1 );
+        ArrayList<String> eastMoves = calculateEastMoves( board, 1 );
+        ArrayList<String> westMoves = calculateWestMoves( board, 1 );
+        ArrayList<String> allMoves = new ArrayList<String>();
+        allMoves.addAll( northEastMoves );
+        allMoves.addAll( northWestMoves );
+        allMoves.addAll( southWestMoves );
+        allMoves.addAll( southEastMoves );
+        allMoves.addAll( northMoves );
+        allMoves.addAll( southMoves );
+        allMoves.addAll( westMoves );
+        allMoves.addAll( eastMoves );
+        return allMoves;
+    }
+    /**
+     * Determines if this King is checked.
+     *
+     * @param board
+     *            the board to check on
+     * @return true if checked, false if not checked
+     */
+    public boolean isChecked( ChessGameBoard board ){
+        return getCurrentAttackers( board ).size() > 0;
+    }
+    /**
+     * Creates an icon for this piece depending on the piece's color.
+     *
+     * @return ImageIcon the ImageIcon representation of this piece.
+     */
+    @Override
+    public ImageIcon createImageByPieceType(){
+        if ( getColorOfPiece() == ChessGamePiece.WHITE ){
+            return new ImageIcon(
+                getClass().getResource("chessImages/WhiteKing.gif")
+            );            
         }
-        return possibleMoves;
-    }
-
-    @Override
-    public boolean isChecked(ChessGameBoard board) {
-        return getCurrentAttackers(board).size() > 0;
-    }
-
-    @Override
-    public ImageIcon createImageByPieceType() {
-        String filename;
-        if (getColorOfPiece() == ChessGamePiece.WHITE) {
-            filename = "WhiteKing.gif";
-        } else if (getColorOfPiece() == ChessGamePiece.BLACK) {
-            filename = "BlackKing.gif";
-        } else {
-            filename = "default-Unassigned.gif";
+        else if ( getColorOfPiece() == ChessGamePiece.BLACK ){
+            return new ImageIcon(
+                getClass().getResource("chessImages/BlackKing.gif" )
+            );            
         }
-        return new ImageIcon(getClass().getResource("chessImages/" + filename));
-    }
-    
-    private boolean isValidMove(int newRow, int newCol) {
-        return getBoard().isOnBoard(newRow, newCol)
-            && (!getBoard().hasPiece(newRow, newCol)
-            || getBoard().getPiece(newRow, newCol).getColorOfPiece() != getColorOfPiece());
-    }
-    
-    private String getPositionString(int row, int col) {
-        return (char) ('a' + col) + "" + (row + 1);
+        else
+        {
+            return new ImageIcon(
+                getClass().getResource("chessImages/default-Unassigned.gif" )
+            );            
+        }
     }
 }
